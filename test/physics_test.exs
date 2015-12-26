4cbdd007b8226217ae3b4738976aa873ee396e72
Levente Mihaly <levente.mihaly@gmail.com>
2015-12-26 15:12:24 +0100

    escape velocity

--- test/physics_test.exs
+++ test/physics_test.exs
@@ -2,10 +2,22 @@ defmodule PhysicsTest do
   use ExUnit.Case
   doctest Physics
 
-  test "the truth" do
-    assert 1 + 1 == 2
-    "the Force may be with " <> somebody = "the Force may be with you"
-    IO.inspect somebody
-    assert somebody == "you"
+  test "convert to km" do
+    assert Calcs.convert_to_km(1000) == 1
   end
+
+  test "rounded to nearest tenth 1" do
+    assert Calcs.rounded_to_nearest_tenth(11.123) == 11.2
+  end
+  
+  test "rounded to nearest tenth 2" do
+    assert Calcs.rounded_to_nearest_tenth(11.192) == 11.2
+  end
+
+  test "earth escape velocity" do
+    earth_ev = Physics.Rocketry.escape_velocity(:earth)
+    assert earth_ev == 11.2
+  end
+  
+  
 end
