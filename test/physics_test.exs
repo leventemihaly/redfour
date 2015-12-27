58a9f0fea08c8fe6f02ac41467aca81afe8706d8
Levente Mihaly <levente.mihaly@gmail.com>
2015-12-27 13:26:34 +0100

    added timex, new test

--- test/physics_test.exs
+++ test/physics_test.exs
@@ -1,5 +1,7 @@
 defmodule PhysicsTest do
   use ExUnit.Case
+  alias Physics.Rocketry
+  
   doctest Physics
 
   test "convert to km" do
@@ -15,29 +17,29 @@ defmodule PhysicsTest do
   end
 
   test "earth escape velocity" do
-    earth_ev = Physics.Rocketry.escape_velocity(:earth)
+    earth_ev = Rocketry.escape_velocity(:earth)
     assert earth_ev == 11.2
   end
   
   test "mars escape velocity" do
-    mars_ev = Physics.Rocketry.escape_velocity(:mars)
+    mars_ev = Rocketry.escape_velocity(:mars)
     assert mars_ev == 5.1
   end
   
   test "moon escape velocity" do
-    moon_ev = Physics.Rocketry.escape_velocity(:moon)
+    moon_ev = Rocketry.escape_velocity(:moon)
     assert moon_ev == 2.4
   end
   
   test "earth orbital acceleartion" do
-   acc = Physics.Rocketry.orbital_acceleration(100)
+   acc = Rocketry.orbital_acceleration(100)
    assert Float.ceil(acc, 2) == 9.52
   end
   
   test "orbital term" do
-   t = Physics.Rocketry.orbital_term(250)
+   t = Rocketry.orbital_term(250)
    IO.inspect(t)
-   km = Physics.Rocketry.orbital_height(4)
+   km = Rocketry.orbital_height(4)
    IO.inspect(km)
   end
 end
